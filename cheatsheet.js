d50535c564a12440082ad6b519e78ff56595bfca
Diego Morales <diegomorales85@gmail.com>
2017-07-23 15:18:09 -0400

    add changing text and style using buttons with js

--- cheatsheet.js
+++ cheatsheet.js
@@ -10,9 +10,19 @@ alert("Message goes here");
 //inline
 onclick="alert("message goes here")";
 
+//Accessing elements with the HTML: 
 
+document.getElementById("the id name").innerHTML = "Use text";
 
+/* 
+document is the actual html you are on. getElementById means go get the of the element, they use the id, innerHTML means get the value of that id and change it. 
+*/
 
+//responding to a click, or a button.
 
+onclick= fucntion () {
+    
+    document.getElementById("name of the ID").innerHTML = "change the text";
+}
 
 
